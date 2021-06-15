566bfd36d0daa127e2f7e5771f223b3d0fdb58e0
wisnufajar <72174900+wisnufajar@users.noreply.github.com>
2021-06-15 19:54:27 +0700

    Add files via upload

---
+++ controllers/home.php
@@ -0,0 +1,87 @@
+<?php
+class home extends Controller
+{
+    public function index()
+    {
+        if (isset($_SESSION['id'])) {
+            $data['judul'] = 'Kenyangin';
+            $data['user'] = $this->model('User_model')->getUserByid($_SESSION['id']);
+            $_SESSION['username'] = $data['user']['username'];
+            $_SESSION['foto'] = $data['user']['foto'];
+            $this->view('templates/header', $data);
+            $this->view('home/index');
+            $this->view('templates/footer');
+        } else {
+            $data['judul'] = 'Kenyangin';
+            $this->view('templates/header', $data);
+            $this->view('home/index');
+            $this->view('templates/footer');
+        }
+    }
+
+    public function masuk()
+    {
+        $data['user'] = $this->model('User_model')->login($_POST);
+        if (isset($data['user']['id'])) {
+            $_SESSION['id'] = $data['user']['id'];
+            if ($this->model('User_model')->updatedAt($data['user']['id']) > 0) {
+                header('Location: ' . BASEURL);
+            } else {
+                Flasher::setFlash('Gagal', 'Masuk', 'danger');
+                header('Location: ' . BASEURL . '/home');
+                exit;
+            }
+        } else {
+            Flasher::setFlash('Gagal', 'Masuk', 'danger');
+            header('Location: ' . BASEURL . '/home');
+            exit;
+        }
+    }
+
+    public function pendaftaran()
+    {
+        $data['judul'] = 'Kenyangin';
+        $this->view('templates/header', $data);
+        $this->view('home/pendaftaran');
+        $this->view('templates/footer');
+    }
+
+    public function keluar()
+    {
+        session_destroy();
+        header('Location: ' . BASEURL);
+        exit;
+    }
+
+    public function daftar()
+    {
+        if ($_POST['password'] == $_POST['cpassword']) {
+            if ($this->model('User_model')->daftar($_POST, $_FILES) > 0) {
+                Flasher::setFlash('Berhasil', 'Daftar', 'success');
+                header('Location: ' . BASEURL);
+                exit;
+            } else {
+                Flasher::setFlash('Gagal', 'Daftar', 'danger');
+                header('Location: ' . BASEURL);
+                exit;
+            }
+        } else {
+            Flasher::setFlash('Gagal', 'Daftar', 'danger');
+            header('Location: ' . BASEURL);
+            exit;
+        }
+    }
+
+    public function lupa()
+    {
+        if ($this->model('User_model')->lupa($_POST['emaill']) > 0) {
+            Flasher::setFlash('Berhasil', 'Silahkan Cek Email', 'success');
+            header('Location: ' . BASEURL);
+            exit;
+        } else {
+            Flasher::setFlash('Gagal', 'Anda Bukan User', 'danger');
+            header('Location: ' . BASEURL);
+            exit;
+        }
+    }
+}
